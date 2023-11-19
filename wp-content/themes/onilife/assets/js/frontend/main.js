7351a441084533e51f9c5672bcdcec76224a58f3
Peter Kimanzi <71552773+peter-kimanzi@users.noreply.github.com>
2023-11-19 14:36:43 +0300

    first commit

---
+++ wp-content/themes/onilife/assets/js/frontend/main.js
@@ -0,0 +1,9 @@
+(function($){'use strict';function login_dropdown(){$('.site-header-account').mouseenter(function(){$('.account-dropdown',this).append($('.account-wrap'));});}
+function handleWindow(){var body=document.querySelector('body');if(window.innerWidth>body.clientWidth+5){body.classList.add('has-scrollbar');body.setAttribute('style','--scroll-bar: '+(window.innerWidth-body.clientWidth)+'px');}else{body.classList.remove('has-scrollbar');}}
+function minHeight(){var $body=$('body'),bodyHeight=$(window).outerHeight(),headerHeight=$('header.header-1').outerHeight(),footerHeight=$('footer.site-footer').outerHeight(),$adminBar=$('#wpadminbar');if($adminBar.length>0){headerHeight+=$adminBar.height();}
+if($body.find('header.header-1').length){$('.site-content').css({'min-height':bodyHeight-headerHeight-footerHeight-120});}}
+function setPositionLvN($item){var sub=$item.children('.sub-menu'),offset=$item.offset(),width=$item.outerWidth(),screen_width=$(window).width(),sub_width=sub.outerWidth();var align_delta=offset.left+width+sub_width-screen_width;if(align_delta>0){if($item.parents('.menu-item-has-children').length){sub.css({left:'auto',right:'100%'});}else{sub.css({left:'auto',right:'0'});}}else{sub.css({left:'',right:''});}}
+function initSubmenuHover(){$('.site-header .primary-navigation .menu-item-has-children').hover(function(event){var $item=$(event.currentTarget);setPositionLvN($item);});}
+function contentHeight(){var heightHideInfo=$('.content-area').outerHeight();$('#secondary').css({minHeight:heightHideInfo});}
+function accordion(){var $accordionitem=$('.elementor-accordion .elementor-accordion-item');$accordionitem.first().addClass("active");$accordionitem.on('click',function(e){e.preventDefault();$accordionitem.removeClass('active');$(this).addClass('active');});}
+accordion();contentHeight();initSubmenuHover();minHeight();handleWindow();login_dropdown();})(jQuery);
---
+++ wp-content/themes/onilife/assets/js/frontend/text-editor.js
@@ -0,0 +1,2 @@
+(function($){'use strict';function text_editor_hover(){let $text_hover=$('.text-editor-hover');$text_hover.each(function(){let $text=$(this).find('.elementor-text-editor > *').text();$(this).find('.elementor-text-editor  > *').attr('data-hover',$text);});}
+text_editor_hover();})(jQuery);
